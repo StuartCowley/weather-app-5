eaab780f5f3fdecb60a9cec7568f67da8385c98f
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2022-03-17 17:24:57 +0000

    updated file structure

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 import React from "react";
-import App from './App';
+import App from "../components/App";
 
-test('renders weather app header', () => {
+test("renders weather app header", () => {
   render(<App />);
   const linkElement = screen.getByText(/weather app/i);
   expect(linkElement).toBeInTheDocument();
