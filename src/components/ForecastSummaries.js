db9068dbaf050d38205da5f3a1b40dbb68c8c0c9
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2022-03-18 10:58:33 +0000

    added return keyword to ForecastSummaries function so that it now renders, corrected PropType errors

--- src/components/ForecastSummaries.js
+++ src/components/ForecastSummaries.js
@@ -3,26 +3,32 @@ import PropTypes from "prop-types";
 import ForecastSummary from "./ForecastSummary";
 
 function ForecastSummaries({ forecasts }) {
-  <div className="forecast-summaries">
-    {forecasts.map((forecast) => (
-      <ForecastSummary
-        key={forecast.date}
-        date={forecast.date}
-        description={forecast.description}
-        icon={forecast.icon}
-        temperature={forecast.temperature}
-      />
-    ))}
-  </div>;
+  return (
+    <div className="forecast-summaries">
+      {forecasts.map((forecast) => (
+        <ForecastSummary
+          key={forecast.date}
+          date={forecast.date}
+          description={forecast.description}
+          icon={forecast.icon}
+          temperature={forecast.temperature}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default ForecastSummaries;
 ForecastSummaries.propTypes = {
-  date: PropTypes.number.isRequired,
-  description: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired,
-  temperature: PropTypes.shape({
-    min: PropTypes.number,
-    max: PropTypes.number,
-  }).isRequired,
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number.isRequired,
+      description: PropTypes.string.isRequired,
+      icon: PropTypes.string.isRequired,
+      temperature: PropTypes.shape({
+        min: PropTypes.number,
+        max: PropTypes.number,
+      }).isRequired,
+    })
+  ).isRequired,
 };
