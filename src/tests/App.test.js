02d2c6abfbc61e37992c50e150d7a5804500c7d4
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2022-03-26 16:40:38 +0000

    Corrected App test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,12 +1,12 @@
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import { render } from "@testing-library/react";
 import App from "../components/App";
-import forecast from "../data/forecast.json";
 
 describe("App", () => {
   test("renders App component correctly", () => {
     // eslint-disable-next-line react/react-in-jsx-scope
-    render(<App location={forecast.location} forecasts={forecast.forecasts} />);
-    const h1Element = screen.getByText(/Manchester, UK/i);
-    expect(h1Element).toBeInTheDocument();
+    const { asFragment } = render(<App />);
+
+    expect(asFragment()).toMatchSnapshot();
   });
 });
